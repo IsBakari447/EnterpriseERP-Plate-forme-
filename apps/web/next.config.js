b62c93bc869cbefbe6d089cb7daefbc3791b5478
Issa Bakari <bakarii447@gmail.com>
2026-08-10 02:44:35 +0200

    Fix web path aliases for Render

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,6 +3,17 @@ const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   outputFileTracingRoot: path.join(__dirname),
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "src"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+      "@modules": path.resolve(__dirname, "src/modules"),
+      "@config": path.resolve(__dirname, "src/config"),
+    };
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
